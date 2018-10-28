f04e102d9bc5f698d6b31cc3b6f0fdcdc7daad02
Brendan Linn <brendan.linn@gmail.com>
2018-10-28 07:58:49 -0700

    packages/react/index.mjs: re-export default export.
    the es build of react-dom starts with a default import of
    react (`import React from 'react'`), so i need to export that too. see
    http://exploringjs.com/es6/ch_modules.html#_re-exporting for the
    syntax.
    
    bump package.json version number for publishing.

--- packages/react/index.mjs
+++ packages/react/index.mjs
@@ -1,3 +1,4 @@
 // TODO: https://github.com/facebook/react/issues/11503
 export {Component, PureComponent} from './src/ReactBaseClasses';
+export {default} from './src/React';
 export * from './src/React';
