4e6e1a19d907b490508596d9ffedc55d8934f881
Brendan Linn <brendan.linn@gmail.com>
2018-10-28 10:07:37 -0700

    packages/react-dom: export render.
    had to bump semver twice because of an error. (0.1.0 re-exported the
    default export, but that's not how downstream code uses it.)

--- packages/react-dom/index.mjs
+++ packages/react-dom/index.mjs
@@ -1,2 +1,5 @@
 // TODO: https://github.com/facebook/react/issues/11503
+import ReactDOM from './src/client/ReactDOM';
+
 export * from './src/client/ReactDOM';
+export const render = ReactDOM.render;
