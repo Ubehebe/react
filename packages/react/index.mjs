f84f9cf7fea7828ad03f734f6995e7e92eb55fd9
Brendan Linn <brendan.linn@gmail.com>
2018-10-29 16:40:54 -0700

    packages/react: export createElement.

--- packages/react/index.mjs
+++ packages/react/index.mjs
@@ -1,4 +1,5 @@
 // TODO: https://github.com/facebook/react/issues/11503
 export {Component, PureComponent} from './src/ReactBaseClasses';
+export {createElement} from './src/ReactElement';
 export {default} from './src/React';
 export * from './src/React';
